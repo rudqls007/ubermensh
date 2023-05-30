1291dd2dfc45d1b40d8a06c1b72990bab027587f
rudqls007 <rudqls007@gmail.com>
2023-05-30 17:44:07 +0900

    Controller의 예외처리

--- jex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
+++ jex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
@@ -1,11 +1,8 @@
 package org.zerock.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -22,12 +19,5 @@ public class CommonExceptionAdvice {
 		log.error(model);
 		return "error_page";
 		
-	}
-	
-	@ExceptionHandler(NoHandlerFoundException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public String handle404(NoHandlerFoundException ex) {
-		
-		return "custom404";
 	}
 }
